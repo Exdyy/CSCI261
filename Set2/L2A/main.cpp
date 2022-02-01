cc431c08286ebbae6dc580ec3957e0287f220d76
Nick <nick.willis13@gmail.com>
2022-02-01 00:11:18 -0700

    L2A Complete

--- Set2/L2A/main.cpp
+++ Set2/L2A/main.cpp
@@ -1,25 +1,60 @@
-using namespace std;
-
 #include <iostream> 
 #include <fstream>
-#include <stdlib.h>
+
+using namespace std;
 
 int main() {
-    ofstream decode("test.txt");
-    ifstream secretMessage("test.txt");
-
-
-    if (secretMessage.fail()) {
-        cerr << "Error opening output file";
+    ofstream decipheredMessage("decipheredMessage.txt");
+    ifstream secretMessage("secretMessage.txt");
+    char c;
+    
+    if (!secretMessage) {
+        cerr << "Error opening input file";
         return -1;
     }
 
-    secretMessage.open("test.txt");
-    if (secretMessage.is_open()) { 
 
-        decode << "Test test test" << endl;
-        secretMessage.close();
+    if (secretMessage.is_open()) {
+        printf("DEBUG: secretMessage is open\n");
+        while (!secretMessage.eof()) {
+            secretMessage.get(c);
+            printf("c: %c\n", c);
+            switch (c) {
+                case '~':
+                printf("DEBUG: case ~\n");
+                if ( decipheredMessage.fail() ) {
+                    cerr << "Error opening output file~";
+                    return -1;
+                }
+                decipheredMessage << ' ';
+                break;
+
+                case '\n':
+                printf("DEBUG: case \\n\n");
+                if ( decipheredMessage.fail() ) {
+                    cerr << "Error opening output file\\n";
+                    return -1;
+                }
+                decipheredMessage << '\n';
+                break;
+
+                default:
+                printf("DEBUG: default case\n");
+                if ( decipheredMessage.fail() ) {
+                    cerr << "Error opening output file-d";
+                    return -1;
+                }
+                cout << "DEBUG: modified c = " << char(c + 2) << '\n';
+                decipheredMessage << (char)(c + 1);
+                break;
+
+            }
         }
+
+    }
+
+        secretMessage.close();
+        decipheredMessage.close();
 
 
         return 0;
