4cd7bc9ab8e458cfc0a8b56d0dd98ba564f26d78
Exdy <nick.willis13@gmail.com>
2022-02-17 00:45:56 -0700

    Commented L3A

--- Set3/L3A/main.cpp
+++ Set3/L3A/main.cpp
@@ -5,24 +5,26 @@ using namespace std;
 
 int main(){
 
-    int userInput = -1;
-    vector<int> nums;
-    int i = 0;
+    int userInput = -1;         //Initialized to -1 to avoid triggering loop exit
+    vector<int> nums;           //Vector to store user inputs
+    int i = 0;                  //Counter initialized to 0
+
     printf("Enter up to 15 non-zero numbers.  Enter zero to signal the end of data or enter all 15 items:\n");
-    while (i<15 && userInput != 0)
+
+    while (i<15 && userInput != 0)              //Loop until 15 iterations, or user inputs zero to quit
     {
         cout << "Number " << i + 1 << ": ";
-        cin >> userInput;
-        if (userInput != 0)
+        cin >> userInput;                       //Get input from user for each integer
+        if (userInput != 0)                     //Loop to append each input to the end of the nums vector
         {
-        nums.push_back(userInput);
+        nums.push_back(userInput);              //Append to vector
         }
-        i++;
+        i++;                                    //Increment counter
     }
     cout << "The numbers are:";
-    for (unsigned int j = 0; j < nums.size(); j++)
+    for (unsigned int j = 0; j < nums.size(); j++)  //Loop once for each integer held in the nums vector
     {
-        cout << " " << nums[j];
+        cout << " " << nums[j];                     //Output values to console in one line, separated by spaces
     }
     return 0;
 }
---
+++ test/test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+signed int x = -1;
+unsigned int y = -1;
+
+cout << x << " " << y << endl;
+
+}
