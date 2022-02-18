c634c62ac319478ca50c5ed8d515f94eee7dd201
Exdy <nick.willis13@gmail.com>
2022-02-18 00:11:00 -0700

    L3A and L3A commented

--- Set3/L3A/main.cpp
+++ Set3/L3A/main.cpp
@@ -3,15 +3,16 @@
 #include <vector>
 using namespace std;
 
-int main(){
-
+int main()
+{
     int userInput = -1;         //Initialized to -1 to avoid triggering loop exit
+    int min, max;               //Initialize min and max integer variables
     vector<int> nums;           //Vector to store user inputs
     int i = 0;                  //Counter initialized to 0
 
     printf("Enter up to 15 non-zero numbers.  Enter zero to signal the end of data or enter all 15 items:\n");
 
-    while (i<15 && userInput != 0)              //Loop until 15 iterations, or user inputs zero to quit
+    while (i < 15 && userInput != 0)            //Loop until 15 iterations, or user inputs zero to quit
     {
         cout << "Number " << i + 1 << ": ";
         cin >> userInput;                       //Get input from user for each integer
@@ -22,9 +23,29 @@ int main(){
         i++;                                    //Increment counter
     }
     cout << "The numbers are:";
-    for (unsigned int j = 0; j < nums.size(); j++)  //Loop once for each integer held in the nums vector
+
+    for (int j = 0; j < nums.size(); j++)       //Loop once for each integer held in the nums vector
     {
-        cout << " " << nums[j];                     //Output values to console in one line, separated by spaces
+        cout << " " << nums[j];                 //Output values to console in one line, separated by spaces
     }
+
+    cout << endl;                               //Add endline to separate outputs  
+    max = nums.at(0);
+    min = nums.at(0);
+
+    for (int k = 0; k < nums.size(); k++)       //Iterate through nums vector to find minimum and maximum values stored
+    {
+        if (nums[k] > max)
+        {
+            max = nums[k];
+        }
+        if (nums[k] < min)
+        {
+            min = nums[k];
+        }
+    }
+    cout << "The maximum value is: " << max << endl;    //Print maximum value to console
+    cout << "The minimum value is: " << min << endl;    //Print minimum value to console
+
     return 0;
 }
