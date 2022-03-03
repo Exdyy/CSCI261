613493d7e205a3487cf4ab6cca8eb035b50f0184
Exdy <nick.willis13@gmail.com>
2022-03-03 16:19:47 -0700

    Fixed L4A - FINISHED

--- Set4/L4A/main.cpp
+++ Set4/L4A/main.cpp
@@ -1,51 +1,74 @@
 #include <iostream>
 #include <cstdlib>
+#include <iomanip>
 using namespace std;
 
 struct Node {
     int value;
-    Node *pNext;
+    Node *pNext = nullptr;
 };
 
 int main(){
 
     //PART 1: COMPLETE USING POINTER/ARRAY
+    cout << "Part 1: Using an array" << endl;
     int newInt;
     int *pX = new int[3];
     for (int i = 0; i < 3; i++){
-        cout << "\nPlease enter an integer: ";
+        cout << setw(28) << "Please enter an integer: ";
         cin >> newInt;
         pX[i] = newInt;
     }
-    cout << "Array contents: ";
+    cout << endl << setw(28) << "Array contents (forward): ";
     for (int i = 0; i < 3; i++){
         cout << pX[i] << ' ';
     }
-    cout << endl;
-    cout << "Backwards array contents: ";
+    cout << endl << setw(28) << "Array contents (backward): ";
     for (int i = 2; i >= 0; i--){
         cout << pX[i] << ' ';
     }
-    cout << endl;
+    cout << endl << endl;
     delete pX;
 
 
     //PART 2: COMPLETE USING A LINKED LIST
     cout << "Part 2: Using a linked list" << endl;
     Node *pHead = new Node;
-    for (int i = 0; i < 3; i++){
-        cout << "Please enter an integer: ";
+    Node *one = new Node;
+    Node *two = new Node;
+
+    pHead->pNext = one;
+    one->pNext = two;
+
+    Node *n = pHead;
+    do {
+        cout << setw(28) << "Please enter an integer: ";
         cin >> newInt;
-        (pHead+i)->value = newInt;
-    }
-    cout << "Array contents: ";
-    for (int i = 0; i < 3; i++){
-        cout << (pHead+i)->value << ' ';
-    }
-    cout << endl << "Backwards array contents: ";
-    for (int i = 2; i >= 0; i--){
-        cout << (pHead+i)->value << ' ';
-    }
-    cout << endl;
+        n->value = newInt;
+        n = n->pNext;
+    } while (n != nullptr);
+
+    n = pHead;
+    cout << endl << setw(28) << "Array contents (forward): ";
+    do {
+        cout << n->value << ' ';
+        n = n->pNext;
+    } while (n != nullptr);
+
+    n = pHead;
+    cout << endl << setw(28) << "Array contents (backward): ";
+    n = n->pNext;
+    n = n->pNext;
+    cout << n->value << ' ';
+    n = pHead->pNext;
+    cout << n->value << ' ';
+    cout << pHead->value;
+
     delete pHead;
+    delete one;
+    delete two;
+    delete n;
+
+
+
 }
